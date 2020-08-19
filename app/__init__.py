843359f6064464e378da8a55151ecc2fcc124ba1
Joseph <dacalculator@gmail.com>
2020-08-19 12:29:24 +1000

    Stabilised connection to Azure

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,33 @@
 import os
+import urllib.parse
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-# Get the directory path of our file
-basedir = os.path.abspath(os.path.dirname(__file__))
+# os.environ['dbstring']
+db_string = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:flaskmd.database.windows.net,1433;Database=flaskmd;Uid=flaskmdadmin;Pwd={Dadisno1};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+params = urllib.parse.quote_plus(db_string)
 
-# Create a path to our sqlite file based on the directory path above
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+params = urllib.parse.quote_plus \
+(r'Driver={ODBC Driver 13 for SQL Server};Server=tcp:flaskmd.database.windows.net,1433;Database=flaskmd;Uid=flaskmdadmin;Pwd={Dadisno1};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine_azure = create_engine(conn_str,echo=True)
+
+# initialization
+#app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'supersecret'
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Wrap our Flask-Alchemy instance around our Flask Application
 db = SQLAlchemy(app)
 
-from app import routes, models
+
+
+from app import routes
 
 @app.before_first_request
 def create_tables():
